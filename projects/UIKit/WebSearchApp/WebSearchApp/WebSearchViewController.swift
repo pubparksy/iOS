cfbfa91a1e96f25fbea1efbe7353bb2ef2a0469f
pubparksy <pubparksy@gmail.com>
2024-09-07 23:37:59 +0900

    [home] WebSearchApp - 미니플젝 데이터 호출 받은 거를 tableView에 출력 테스트 (app Key 제외)

--- projects/UIKit/WebSearchApp/WebSearchApp/WebSearchViewController.swift
+++ projects/UIKit/WebSearchApp/WebSearchApp/WebSearchViewController.swift
@@ -16,13 +16,18 @@ class WebSearchViewController: UITableViewController {
     
     var wItemKey = ["title","link","description"]
     var iItemKey = ["title","link","thumbnail","sizeheight","sizewidth"]
-    var vItemKey = ["title","author","play_time","url","datetime"]
+    var vItemKey = ["title","author","play_time","datetime","url","thumbnail"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
         search("Jazz")
+        
     }
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
 
     func search(_ query:String) {
         let endPoint1 = "https://openapi.naver.com/v1/search/webkr.json?query=\(query)&display=3"
@@ -55,7 +60,7 @@ class WebSearchViewController: UITableViewController {
             
             do {
                 guard let root = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any],
-                      let webList = root["items"] as? [[String:Any]]
+                      let wList = root["items"] as? [[String:Any]]
                                 else { return }
 //                print(webList)
 //                print(webList[0]["title"] as? String)
@@ -70,20 +75,20 @@ class WebSearchViewController: UITableViewController {
 //                print(wDesc)
 //                print()
                 
-                print("[다음 영상 검색]")
-                for web in webList {
-                    guard let wTitle = web[self.wItemKey[0]],
-                          let wLink  = web[self.wItemKey[1]],
-                          let wDesc  = web[self.wItemKey[2]]
-                    else { return }
-                    print(wTitle)
-                    print(wLink)
-                    print(wDesc)
-                    print()
-                }
-                print()
+//                print("[다음 영상 검색]")
+//                for web in webList {
+//                    guard let wTitle = web[self.wItemKey[0]],
+//                          let wLink  = web[self.wItemKey[1]],
+//                          let wDesc  = web[self.wItemKey[2]]
+//                    else { return }
+//                    print(wTitle)
+//                    print(wLink)
+//                    print(wDesc)
+//                    print()
+//                }
+//                print()
                 
-                
+                self.webList = wList
                 
                 
                 let task2 = session.dataTask(with: request2) { data, response, error in
@@ -92,7 +97,7 @@ class WebSearchViewController: UITableViewController {
                     
                     do {
                         guard let root = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any],
-                             let imgList = root["items"] as? [[String:Any]]
+                             let iList = root["items"] as? [[String:Any]]
                                         else { return }
 //                        print(items)
 //                        print()
@@ -107,18 +112,19 @@ class WebSearchViewController: UITableViewController {
 //                        print()
                         
                         
-                        print("[네이버 이미지 검색]")
-                        for img in imgList {
-                            guard let iTitle = img[self.iItemKey[0]],
-                                  let iLink  = img[self.iItemKey[1]],
-                                  let iDesc  = img[self.iItemKey[2]]
-                            else { return }
-                            print(iTitle)
-                            print(iLink)
-                            print(iDesc)
-                            print()
-                        }
-                        print()
+//                        print("[네이버 이미지 검색]")
+//                        for img in imgList {
+//                            guard let iTitle = img[self.iItemKey[0]],
+//                                  let iLink  = img[self.iItemKey[1]],
+//                                  let iDesc  = img[self.iItemKey[2]]
+//                            else { return }
+//                            print(iTitle)
+//                            print(iLink)
+//                            print(iDesc)
+//                            print()
+//                        }
+//                        print()
+                        self.imgList = iList
                         
                         let task3 = session.dataTask(with: request3) { data, response, error in
                             if let error { print("\(error) 발생"); return }
@@ -126,7 +132,7 @@ class WebSearchViewController: UITableViewController {
                             
                             do {
                                 guard let root     = try JSONSerialization.jsonObject(with: data, options: []) as? [String:Any],
-                                     let vdList = root["documents"] as? [[String:Any]]
+                                     let vList = root["documents"] as? [[String:Any]]
                                                 else { return }
 //                                print(vdList)
                                 
@@ -145,24 +151,30 @@ class WebSearchViewController: UITableViewController {
 //                                print()
                                 
                                 
-                                print("[다음 영상 검색]")
-                                for vd in vdList {
-                                    guard let vTitle    = vd[self.vItemKey[0]],
-                                          let vAuthor   = vd[self.vItemKey[1]],
-                                          let vPlayTime = vd[self.vItemKey[2]],
-                                          let vUrl      = vd[self.vItemKey[3]],
-                                          let vDateTime = vd[self.vItemKey[4]]
-                                    else { return }
-                                    print(vTitle)
-                                    print(vAuthor)
-                                    print(vPlayTime)
-                                    print(vUrl)
-                                    print(vDateTime)
-                                    print()
+//                                print("[다음 영상 검색]")
+//                                for vd in vdList {
+//                                    guard let vTitle    = vd[self.vItemKey[0]],
+//                                          let vAuthor   = vd[self.vItemKey[1]],
+//                                          let vPlayTime = vd[self.vItemKey[2]],
+//                                          let vUrl      = vd[self.vItemKey[3]],
+//                                          let vDateTime = vd[self.vItemKey[4]]
+//                                    else { return }
+//                                    print(vTitle)
+//                                    print(vAuthor)
+//                                    print(vPlayTime)
+//                                    print(vUrl)
+//                                    print(vDateTime)
+//                                    print()
+//                                }
+//                                print()
+                                
+                                self.vdList = vList
+                                
+                                
+                                /** 테이블 구조 설정 했는데도 안나오면 이거 안한 것 */
+                                DispatchQueue.main.async {
+                                    self.tableView.reloadData()
                                 }
-                                print()
-                                
-                                
                                 
                             } catch {
                                 print(error)
@@ -187,4 +199,139 @@ class WebSearchViewController: UITableViewController {
         task1.resume()
         
     }
+    
+    
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+//        print("numberOfSections")
+        return 3
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+            case 0: return webList?.count ?? 0
+            case 1: return imgList?.count ?? 0
+            case 2: return vdList?.count ?? 0
+        default: return 0
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        if indexPath.section == 0 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "web", for: indexPath)
+            guard let web = webList?[indexPath.row] else { return cell }
+            
+            let wTitle = cell.viewWithTag(11) as? UILabel
+            wTitle?.text = web[self.wItemKey[0]] as? String
+            let wDesc = cell.viewWithTag(12) as? UILabel
+            wDesc?.text = web[self.wItemKey[1]] as? String
+            
+        } else if indexPath.section == 1 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "img", for: indexPath)
+            guard let img = imgList?[indexPath.row] else { return cell }
+            
+            let iTitle = cell.viewWithTag(22) as? UILabel
+            iTitle?.text = img[self.iItemKey[0]] as? String
+            let iDesc  = cell.viewWithTag(23) as? UILabel
+            iDesc?.text = img[self.iItemKey[1]] as? String
+            
+            let iImage = cell.viewWithTag(21) as? UIImageView
+            // 사진 데이터 값이 url로 되어있으니까, 그 url에 1번 더 Session 연결해서 이미지를 가져와야함
+            if let iLink = img[self.iItemKey[1]] as? String {
+                if let url = URL(string: iLink) {
+                    let request = URLRequest(url: url)
+                    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                        if let data {
+                            DispatchQueue.main.sync {
+                                iImage?.image = UIImage(data: data)
+                            }
+                        }
+                    }
+                    task.resume()
+                    
+                }
+            }
+            
+        } else if indexPath.section == 2 {
+            cell = tableView.dequeueReusableCell(withIdentifier: "vd", for: indexPath)
+            guard let vd = vdList?[indexPath.row] else { return cell }
+            
+            let vTitle = cell.viewWithTag(32) as? UILabel
+            vTitle?.text = vd[self.vItemKey[0]] as? String
+            
+            let vDate = cell.viewWithTag(33) as? UILabel
+            if let date = vd[self.vItemKey[3]] as? String {
+                vDate?.text = String(date[date.startIndex ..< date.index(date.startIndex, offsetBy: 10)])
+            }
+            
+            let vAuthor = cell.viewWithTag(34) as? UILabel
+            vAuthor?.text = vd[self.vItemKey[1]] as? String
+            let vPlayTime = cell.viewWithTag(35) as? UILabel
+            var time = ""
+            if let playTime = vd[self.vItemKey[2]] as? Int {
+                if playTime/3600 > 0 {
+                    time = String(playTime/3600) + ":"
+                    if (playTime%3600)/60 > 0 {
+                        time = time + String((playTime%3600)/60) + ":"
+                        time = time + String((playTime%3600)%60)
+                    } else {
+                        time = time + String((playTime%3600)%60)
+                    }
+                } else {
+                    if (playTime%3600)/60 > 1 {
+                        time = time + String((playTime%3600)/60) + ":"
+                        time = time + String((playTime%3600)%60)
+                    } else {
+                        time = time + String(playTime)
+                    }
+                }
+            }
+            vPlayTime?.text = time
+            
+            
+            let vImage = cell.viewWithTag(31) as? UIImageView
+            // 사진 데이터 값이 url로 되어있으니까, 그 url에 1번 더 Session 연결해서 이미지를 가져와야함
+            if let vThumbnail = vd[self.vItemKey[5]] as? String {
+                if let url = URL(string: vThumbnail) {
+                    let request = URLRequest(url: url)
+                    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                        if let data {
+                            DispatchQueue.main.sync {
+                                vImage?.image = UIImage(data: data)
+                            }
+                        }
+                    }
+                    task.resume()
+                }
+            }
+            
+            
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+            case 0: return "Naver Web"
+            case 1: return "Naver Image"
+            case 2: return "Kakao Video"
+        default: return ""
+        }
+    }
+ 
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+            case 0: return 80
+            case 1: return 100
+            case 2: return 260
+        default: return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        50
+    }
+    
 }
